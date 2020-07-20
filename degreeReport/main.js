468b244e9a20a86c6c34a44bb50894dc79f876d6
WeatherApp <alexmflores99@hotmail.ca>
2020-07-20 14:27:12 -0400

    improved code readability
    Added more meaningful descriptions and variable names

--- degreeReport/main.js
+++ degreeReport/main.js
@@ -1,36 +1,24 @@
-
-
 chrome.runtime.onMessage.addListener((request) => {
   var tables = document.querySelectorAll("table"); //gets an array of table elements
-  var lastTable ;
-  if(tables.length <= 8){
+  var currentTable = tables.length <= 8 ? tables[6] : tables[tables.length - 1]; //  gets table with all grades and courses
 
-lastTable = tables[6];
+  var tbody = currentTable.getElementsByTagName("tbody")[0]; // gets tbody element with nested  tb and td elements inside
+  var size = tbody.querySelectorAll("tr").length; // number of tr elements or table rows
 
-
-  }
-  else{
-   lastTable = tables[tables.length - 1]; // retrieves last table
-  }
-  var tbody = lastTable.getElementsByTagName("tbody")[0]; // gets tbody element with nested  tb and td elements inside
-  var size = tbody.querySelectorAll("tr").length;
-  var courses = []; // array of td elements
-  var grades = [];
-  var courseDictionary = new Set();
-
-  /*@desc: copies all courses and their corresponding grade  in tbody to td and grades array respectively.
-           td elements are nested inside tbody array. Only the 2nd td element gets added and 3rd td element 
-           to td and grades array respectively. That is where the grades letter are and course name.
-           */
-  //@param: @size is decreased by 1 so as to not include the first element in tbody array
-
-  var temp = tbody.getElementsByTagName("tr")[1];
-  var start = 0;
-  var textInTdElement = temp
+  var secondTableRow = tbody.getElementsByTagName("tr")[1];
+  var tdPositionWithGrades = 0;
+  var textInTdElement = secondTableRow
     .getElementsByTagName("td")[2]
-    .innerHTML.replace(/ /g, "");
+    .innerHTML.replace(/ /g, ""); // gets ride of white spaces in a string
 
-  start = textInTdElement.length > 4 ? 3 : 2; // checks which td has the grade letter depending if site is DPG or gradesYorku
+  tdPositionWithGrades = textInTdElement.length > 4 ? 3 : 2; // checks which td has the grade letter depending if site is DPG or gradesYorku
+
+  var courses = [];
+  var grades = [];
+  var courseDictionary = new Set(); // stores all unique courses to validate user-input
+
+  //@desc: copies all courses and their corresponding grade  in tbody to td and grades array respectively.
+  //@param: @size is decreased by 1 so as to not include the first element in tbody array
 
   for (var i = 0; i < size - 1; i++) {
     var tr;
@@ -38,7 +26,7 @@ lastTable = tables[6];
 
     courses[i] = tr.getElementsByTagName("td")[1];
 
-    grades[i] = tr.getElementsByTagName("td")[start];
+    grades[i] = tr.getElementsByTagName("td")[tdPositionWithGrades];
 
     var courseCodeOnly = courses[i].innerHTML.substring(0, 7).replace(/ /g, "");
     courseDictionary.add(courseCodeOnly);
@@ -48,74 +36,82 @@ lastTable = tables[6];
 
   var eecsGrades = [];
   var index = 0;
+  var temp;
   const COURSE_CODE = request.toUpperCase().replace(/ /g, "");
 
   /*@desc: Creates same course code but without "/" character 
-  @note: Due to webpage having different course code format, must check for both options for consistency
+  @note: Due to webpage having different course code format, must check for both options for input consistency
   */
-  
+
   var temporaryCourseCode = "";
-  for(var i=0;i < COURSE_CODE.length;i++){
-    
-    if (COURSE_CODE.charAt(i) != "/"){
-      
-      temporaryCourseCode+= COURSE_CODE.charAt(i);
-      
+  for (var i = 0; i < COURSE_CODE.length; i++) {
+    if (COURSE_CODE.charAt(i) != "/") {
+      temporaryCourseCode += COURSE_CODE.charAt(i);
     }
-    
   }
 
   var gradesAndCourses = [];
   var count = 0;
 
-  if (courseDictionary.has(COURSE_CODE) || courseDictionary.has(temporaryCourseCode)){
-    /*@desc: Copies all elements in td array that have the course code LE/EECS to eecsCourses array
+  if (
+    courseDictionary.has(COURSE_CODE) ||
+    courseDictionary.has(temporaryCourseCode)
+  ) {
+    /*@desc: Copies all elements in td array that have the course code @COURSE_CODE to eecsCourses array
   and  adds its  corresponding grade letter  to eecsGrades array.
-  @note: substring is used to only compare the course code then if it is an eecs course the whole course name gets added.
+  @note: substring is used to only compare the course code then if it is the query course the whole course name gets added.
   */
 
     for (var i = 0; i < courses.length; i++) {
       temp = courses[i].innerHTML.substring(0, 7).replace(/ /g, "");
       var gradeLetter = grades[i].innerHTML;
-      if (temp === COURSE_CODE || temp=== temporaryCourseCode) {
+      if (temp === COURSE_CODE || temp === temporaryCourseCode) {
         temp = courses[i].innerHTML;
         eecsCourses[index] = temp;
         eecsGrades[index] = gradeLetter.replace(/ /g, ""); // gets rid of all whitespaces
         index++;
       }
     }
+//@desc: updates the parameters @startPoint & @endPoint's value depending on the course webpage
+//and if the ending character in course code includes an invalid character
+//@param: @startPoint & @endPoint is the index to be started and end from in @eecsCourses inner HTML string respectively
 
-    //@desc: calculates eecs grade
-
-   
     var startPoint = 0;
     var endPoint = 0;
- 
-    var toBeCompared = eecsCourses[0].replace(/ /g,"");
-if(isNaN(toBeCompared.substr(toBeCompared.length-4))){
-startPoint = 5;
-endPoint = 1;
 
-} else{
-startPoint = 4;
-endPoint = 0;
+    var toBeCompared = eecsCourses[0].replace(/ /g, "");
+    if (isNaN(toBeCompared.substr(toBeCompared.length - 4))) {
+      startPoint = 5;
+      endPoint = 1;
+    } else {
+      startPoint = 4;
+      endPoint = 0;
+    }
 
-}
-
+  /* @desc: calculates @COURSE_CODE culminative gpa using the formula: 
+  Graderesult = Gradepoint_N * Creditvalue_N + Gradepoint_N-1 * Creditvalue_N-1...
+  total = Graderesult / total_Gradepoint
+  
+  */
+  
     var eecsGpa = 0;
     var totalCredits = 0;
-var totalGPAPoints =0;
+    var totalGPAPoints = 0;
+ 
     for (var i = 0; i < eecsCourses.length; i++) {
       var courseName = eecsCourses[i].replace(/ /g, ""); // gets rid of all white spaces
       var gradeLetter = eecsGrades[i];
 
-      var courseCredit = courseName.substring(courseName.length-startPoint,courseName.length - endPoint); //grabs the digit of the credit from courseName ex: 3.00 from LE/EECSXXXX3.00
-
+      var courseCredit = courseName.substring(
+        courseName.length - startPoint,
+        courseName.length - endPoint
+      ); //grabs the digit of the credit from courseName ex: 3.00 from LE/EECSXXXX3.00
 
       var gradePoint = gradeLetterToNumber(gradeLetter);
 
       if (gradePoint >= 0) {
-        gradesAndCourses[count++] = eecsCourses[i] + " grade: " + eecsGrades[i] + " ";
+        gradesAndCourses[count++] =
+          eecsCourses[i] + " grade: " + eecsGrades[i] + " ";
         var creditValue = parseInt(courseCredit);
         var result = gradePoint * creditValue;
         eecsGpa += result;
@@ -125,7 +121,17 @@ var totalGPAPoints =0;
     totalGPAPoints = eecsGpa;
     //@desc: Outputs eecs gpa to console and formula used is : total points/total credits
     eecsGpa = (eecsGpa / totalCredits).toFixed(2);
-    alert(COURSE_CODE + " gpa: " + eecsGpa +  " total credits: " + totalCredits +   "\ntotal grade points: " + totalGPAPoints +"\ncourses: \n"+ gradesAndCourses);
+    alert(
+      COURSE_CODE +
+        " gpa: " +
+        eecsGpa +
+        " total credits: " +
+        totalCredits +
+        "\ntotal grade points: " +
+        totalGPAPoints +
+        "\ncourses: \n" +
+        gradesAndCourses
+    );
 
     //@desc: Converts grade letter to equivalent grade point/number according to school's grade system.
 
@@ -143,8 +149,7 @@ var totalGPAPoints =0;
       //Case if course grade is P or RLF
       else return -1;
     }
-  }
-  else   {
+  } else {
     alert("Invalid response. Check spelling of course name");
-  } 
+  }
 });
