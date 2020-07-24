0688d93070035aafe206525d10a19eaef402de63
WeatherApp <alexmflores99@hotmail.ca>
2020-07-24 18:30:58 -0400

    Update main.js
    Added more meaningful comments

--- degreeReport/main.js
+++ degreeReport/main.js
@@ -1,9 +1,9 @@
 chrome.runtime.onMessage.addListener((request) => {
   var tables = document.querySelectorAll("table"); //gets an array of table elements
-  var currentTable = tables.length <= 8 ? tables[6] : tables[tables.length - 1]; //  gets table with all grades and courses
+  var currentTable = tables.length <= 8 ? tables[6] : tables[tables.length - 1]; //  gets table with all grades and courses; 8 is table on grades yorku ,and [6] is from DPR(DegreeProgesReport)
 
   var tbody = currentTable.getElementsByTagName("tbody")[0]; // gets tbody element with nested  tb and td elements inside
-  var size = tbody.querySelectorAll("tr").length; // number of tr elements or table rows
+  const TABLE_SIZE = tbody.querySelectorAll("tr").length; // number of tr elements or table rows in table element
 
   var secondTableRow = tbody.getElementsByTagName("tr")[1];
   var tdPositionWithGrades = 0;
@@ -12,9 +12,9 @@ chrome.runtime.onMessage.addListener((request) => {
     .innerHTML.replace(/ /g, ""); // gets ride of white spaces in a string
 
   tdPositionWithGrades = textInTdElement.length > 4 ? 3 : 2; // checks which td element has the grade letter depending if site is DPG or gradesYorku
-  var terms = [];
-  var courses = [];
-  var grades = [];
+  var terms = []; // stores td element innerHtml of school term 
+  var courses = []; // stores td element with course name not the actual string value
+  var grades = [];// stores td element with course grade not the actual string value
   var courseDictionary = new Set(); // stores all unique courses to validate user-input
 
   /*@desc: copies all courses and their corresponding grade  in @tbody to @courses and @grades array respectively.
@@ -22,29 +22,27 @@ chrome.runtime.onMessage.addListener((request) => {
   */
 
   //@param: @size is decreased by 1 so as to not include the first element in @tbody array
-  for (var i = 0; i < size - 1; i++) {
+  for (var i = 0; i < TABLE_SIZE - 1; i++) {
     var tr2;
-    tr2 = tbody.getElementsByTagName("tr")[i + 1];
+    tr2 = tbody.getElementsByTagName("tr")[i + 1]; // gets specific tr elementfrom table  
 
-    courses[i] = tr2.getElementsByTagName("td")[1];
+    courses[i] = tr2.getElementsByTagName("td")[1]; // gets specific td element with course name from previous tr element
 
-    grades[i] = tr2.getElementsByTagName("td")[tdPositionWithGrades];
+    grades[i] = tr2.getElementsByTagName("td")[tdPositionWithGrades]; // gets td element with grade letter
     terms[i] = tr2.getElementsByTagName("td")[0].innerHTML;
     var courseCodeOnly = courses[i].innerHTML.substring(0, 7).replace(/ /g, "");
     courseDictionary.add(courseCodeOnly);
   }
 
-  var course_code_courses = [];
+  var course_code_courses = []; // stores all unique courses with @COURSE_CODE
 
-  var course_code_grades = [];
+  var course_code_grades = []; // stores all unique grades corresponding to course with @COURSE_CODE
   var index = 0;
   var temp;
-  const COURSE_CODE = request.toUpperCase().replace(/ /g, "");
+  const COURSE_CODE = request.toUpperCase().replace(/ /g, ""); // User response (course gpa to be evaluated)
 
-  /*@desc: Creates same course code but without "/" character 
-  @note: Due to webpage having different course code format, must check for both options for input consistency
-  */
-
+  //@desc: Creates same course code but without "/" character 
+  //@note: Due to webpage having different course code format, must check for both options for input consistency
   var temporaryCourseCode = "";
   for (var i = 0; i < COURSE_CODE.length; i++) {
     if (COURSE_CODE.charAt(i) != "/") {
@@ -57,11 +55,11 @@ chrome.runtime.onMessage.addListener((request) => {
 
   var queryGPAState = "Sorry. Choice of GPA is not available. Please check spelling.\n";
 
-  // GLOBAL STATE 1 @startpoint and @endpoint (used code prevent redudnat code)
+  // GLOBAL STATE 1 @startpoint and @endpoint (used code prevent redudant code)
   //@desc: updates the parameters @startPoint & @endPoint's value depending on the course webpage
   // if the ending character in course code includes an invalid character in the <td> element
-  //@param: @startPoint & @endPoint is the index to be started and end from in @eecsCourses inner HTML string respectively
-
+  //@param: @startPoint & @endPoint is the index to be started and end from in @courses_code_courses inner HTML string respectively
+// Needed to find where the credit is in the string
   var startPoint = 0;
   var endPoint = 0;
 
@@ -80,6 +78,7 @@ chrome.runtime.onMessage.addListener((request) => {
     endPoint = 0;
   }
   // END OF GLOBAL STATE 1
+  //@desc: Validates if user's response is an actual course that they have in their schedule
   if (
     courseDictionary.has(COURSE_CODE) ||
     courseDictionary.has(temporaryCourseCode)
@@ -109,6 +108,7 @@ chrome.runtime.onMessage.addListener((request) => {
     var course_code_total_credits = 0;
     var course_code_GPA = 0;
 
+    //@desc: Calculates user's choice of gpa
     for (var i = 0; i < course_code_courses.length; i++) {
       var courseName = course_code_courses[i].replace(/ /g, ""); // gets rid of all white spaces
       var gradeLetter = course_code_grades[i];
@@ -171,7 +171,7 @@ chrome.runtime.onMessage.addListener((request) => {
 
   var all_credits = [];
   var all_grades = [];
-
+//@desc: Copies all grades and credits string value to @all_grades and @all_credits  respectively
   for (var i = 0; i < courses.length; i++) {
     let text = courses[i].innerHTML.replace(/ /g, "");
     let credit = text.substring(
@@ -214,27 +214,29 @@ chrome.runtime.onMessage.addListener((request) => {
     totalPoints +
     "\n" + "\n";
 
-  // ---------------- Sessional GPA -----------------
-
+  // ---------------- Sessional GPA -------------------
+//@desc: Gets current year  and checks if it matches school's year term. Adjusted year if neccesary
+//to match school's year term. Depending on website user visits, DPR will have most recent school term
+// from the bootom of the table while yorku grades will have it at the top.
   var y = new Date().getFullYear() + "";
   var year = y.substring(2, 4);
 
   if (tdPositionWithGrades === 3) {
-    // sets year term to correct value for school term if ahead by 1 so it updates yorku grades
+    
 
     var t = tbody.getElementsByTagName("tr")[1];
 
     temp = t.getElementsByTagName("td")[0];
    
-    if (parseInt(temp.innerHTML.substring(2, 6))< parseInt(  year)) { // 2020 < 2020
+    if (parseInt(temp.innerHTML.substring(2, 6))< parseInt(  year)) { 
       var temp2 = "" + (new Date().getFullYear() - 1);
       year = temp2.substring(2, 4);
     }
   }
-// Year goes to 2020 for DPR
+
   if (tdPositionWithGrades === 2) {
-    if(size-1 > 0){
-   var  t = tbody.getElementsByTagName("tr")[size-1];// last tr element
+    if(TABLE_SIZE-1 > 0){
+   var  t = tbody.getElementsByTagName("tr")[TABLE_SIZE-1];// last tr element
     temp = t.getElementsByTagName("td")[0];
     
     if (parseInt(temp.innerHTML.substring(2, 6)) < parseInt(year)) {
@@ -243,6 +245,8 @@ chrome.runtime.onMessage.addListener((request) => {
     }
   }
   }
+
+ 
   const TERM_YEAR = "FW" + year;
   const SUMMER_TERM = "SU" + year;
   var FWtermCourseCredits = [];
@@ -250,9 +254,9 @@ chrome.runtime.onMessage.addListener((request) => {
   var FWGrades = [];
   var SUGrades = [];
 
-  var k = 0;
-  var j = 0; //WORKSSS
-  //stores all courses corresponding to term
+  var k = 0;//index for Fall/Winter term arrays
+  var j = 0; //index for Summer term arrays
+  //@desc: stores all courses and grades corresponding to the school term into their respective arrays
   for (var i = 0; i < courses.length; i++) {
     let term = terms[i];
     temp = courses[i].innerHTML;
@@ -263,7 +267,7 @@ chrome.runtime.onMessage.addListener((request) => {
 
     let gradeLetter = grades[i].innerHTML.substring(0, 2);
     courseText = courseText.replace(/ /g, "");
-    if (term == TERM_YEAR && (gradeLetter != "") & (gradeLetter != null)) {
+    if (term == TERM_YEAR && (gradeLetter != "") & (gradeLetter != null)) {// checks if table element  tr element's innerHtml term matches school term
       FWtermCourseCredits[k] = courseText.substring(
         courseText.length - startPoint,
         courseText.length - endPoint
@@ -281,7 +285,7 @@ chrome.runtime.onMessage.addListener((request) => {
       j++;
     }
   }
-  //calculares FW term sessional GPA
+  //@desc: Calculatess FW term sessional GPA
   var FWsessionalGPA = 0;
   var FWsessionalCredits = 0;
   var FWsessionalPoints = 0;
@@ -313,7 +317,7 @@ chrome.runtime.onMessage.addListener((request) => {
     FWsessionalPoints +
     "\n" + "\n";
 
-  // Calculates SU sessional GPA if any SU courses
+  //@desc: Calculates SU sessional GPA if any SU courses
 
   var SUsessionalGPA = 0;
   var SUsessionalCredits = 0;
