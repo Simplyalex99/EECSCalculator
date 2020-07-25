eb94d35a87d908ea6f638fe53b1f024c5bbce893
WeatherApp <alexmflores99@hotmail.ca>
2020-07-25 09:09:58 -0400

    Changed UI layout
    Added more padding and margin between text, and change positioning of buttons for ease of reading. Changed description and color of buttons for UI/UX

--- degreeReport/EECSCalculator.js
+++ degreeReport/EECSCalculator.js
@@ -19,3 +19,25 @@ document.addEventListener(
 
   false
 );
+
+
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  document.getElementById('redirect-btn').addEventListener('click', clickHandler);
+function clickHandler (e) {
+
+chrome.tabs.update({url: "https://w6prod.sis.yorku.ca/yda/student"});
+window.close();
+
+
+}
+
+});
+/*
+
+Add option to direct user to DPR or grades yorku in button. Make sure to make the formatting of buttons 
+and instructions clear and simple so user know in a blink of an eye what to do.
+
+
+*/
