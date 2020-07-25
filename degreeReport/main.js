80d1e8887c49fdf58b71481d324af6b4962b987d
WeatherApp <alexmflores99@hotmail.ca>
2020-07-24 21:15:04 -0400

    Adjusted output for non-query input
    Expanded on error text so users can input correct value if needed

--- degreeReport/main.js
+++ degreeReport/main.js
@@ -53,7 +53,8 @@ chrome.runtime.onMessage.addListener((request) => {
   var gradesAndCourses = [];
   var count = 0;
 
-  var queryGPAState = "Sorry. Choice of GPA is not available. Please check spelling.\n";
+  var queryGPAState = "Sorry. Choice of GPA is not available. Please check spelling\n" +
+  "or enter one as specificied in the description.\n";
 
   // GLOBAL STATE 1 @startpoint and @endpoint (used code prevent redudant code)
   //@desc: updates the parameters @startPoint & @endPoint's value depending on the course webpage
